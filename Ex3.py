e20e8dd71b72099b15095bca8a65b0f129521780
nikita_danshin <listopadobvka1@mail.ru>
2021-12-16 15:06:14 +0300

    add conftest file

--- Ex3.py
+++ Ex3.py
@@ -1,19 +1,26 @@
 from selenium import webdriver
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.by import By
 import time
 import math
 
-link = "http://suninjuly.github.io/huge_form.html"
+link = "https://stepik.org/lesson/236895/step/1"
 try:
     browser = webdriver.Chrome()
     browser.get(link)
-    elements = browser.find_elements_by_tag_name("input")
-    for element in elements:
-        element.send_keys("Кот")
+    browser.implicitly_wait(5)
+    input_1 = browser.find_element_by_css_selector("textarea[spellcheck='false']")
+    answer = str(math.log(int(time.time())))
+    input_1.send_keys(answer)
 
-    button = browser.find_element_by_css_selector("button.btn")
-    button.click()
+    button_1 = WebDriverWait(browser, 10).until(
+        EC.element_to_be_clickable((By.CLASS_NAME, "submit-submission")))
+
+    # button_1 = browser.find_element_by_class_name("submit-submission")
+    button_1.click()
 
 finally:
-    time.sleep(30)
+    time.sleep(100)
     # закрываем браузер после всех манипуляций
     browser.quit()
